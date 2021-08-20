abf21e9dae73d411603a42c7582f3d2860bd79bd
sinaya naik <sinayanaik@gmail.com>
2021-08-20 13:25:08 +0530

    added break point

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     new Splide('.splide', {
         type: 'loop',
         perPage: 3,
+        autoHeight: true,
+        breakpoints: {
+            640: {
+                perPage: 1,
+            },
+        }
     }).mount();
 });
